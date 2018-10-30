7f4318119c259d3c181a9ea7fa71a76aa39069f7
Danielle Dookie <danielledookie90@gmail.com>
2018-10-30 23:36:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let tracker = 0
+
+function onKeyDown(event){
+  const keycode = event.which
+  
+  if (keycode === code[tracker]) {
+    tracker = tracker + 1
+  } else {
+    tracker = 0
+  }
+  
+  if (tracker === 10){
+  alert('you did it')
+  }
+  
+  console.log(tracker)
+  
+  console.log(event)
+  
+}
+
 function init() {
   // Write your JavaScript code inside the init() function
 
+document.body.addEventListener('keydown', onKeyDown)
 }
+
+init()
