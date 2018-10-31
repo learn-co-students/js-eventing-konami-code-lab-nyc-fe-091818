d928cd34cabe8dbb8f212fb6bfc0ce9548b9f37b
Jesse Evans <jesseelizabeth1@gmail.com>
2018-10-31 01:02:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let tracker = 0
+
+function onKeyDown(event) {
+  const keyCode = event.which || event.location
+  
+// 1. figure out where in the sequence they are 
+  if (keyCode === code[tracker]) {
+    tracker = tracker + 1
+  } else {
+    // 2. reset to beggining if they mess up
+    tracker = 0
+  }
+  
+  // 3. show a success message if they win
+  if (tracker === 9) {
+    alert('you did it')
+  }
+}
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', onKeyDown)
 }
+
+init()
